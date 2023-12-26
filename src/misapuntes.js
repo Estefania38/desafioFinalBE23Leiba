5c6d5f841dccda55e4348f71a0c21bc8bf265801
Estefania38 <leibaestefania@gmail.com>
2023-12-26 02:33:11 -0300

    modificando gitignore

--- src/misapuntes.js
+++
@@ -1,9 +0,0 @@
-// 11/10/22
-
-// probando los enlases a las rutas y la funcionalidad de products:
-
-// get de products http://localhost:8080/api/products/ funciona ok
-
-// la ruta de router.get("/:pid", ProductsController.getProductById);  funciona ok
-
-// la ruta de 
